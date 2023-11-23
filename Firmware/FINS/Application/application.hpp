6e5a20c304a03966d0d601be653feeba94b49702
Aaron Hoffman <ajh269@uakron.edu>
2023-11-22 19:56:03 -0500

    Make new workspace and FINS

---
+++ Firmware/FINS/Application/application.hpp
@@ -0,0 +1,16 @@
+#ifndef APPLICATION_HPP
+#define APPLICATION_HPP
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void setup();
+void loop();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APPLICATION_HPP */
