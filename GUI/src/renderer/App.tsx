273a83243837997d72e9968a95011e42d3cc3d29
Aaron Hoffman <ajh269@uakron.edu>
2023-11-28 19:14:17 -0500

    Remove GUI from FINS

--- GUI/src/renderer/App.tsx
+++
@@ -1,6 +0,0 @@
-// Driver for displaying something.
-// In general, all components should be in organized directories
-
-import { Hello } from 'components/Hello/Hello';
-
-export const App = () => <Hello />;
